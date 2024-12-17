44b5da6252ca2e14b2eb7126a4dd028cffdb89d1
Anthony Balderas <117970731+Anthonykyut@users.noreply.github.com>
2024-12-17 18:55:00 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Skills from "./components/Skills"
+import Button from "./components/Button"
+
+
+function App(){
+    return(
+        <div> 
+        <Header/>
+        <Hero/>
+        <About/>
+        <Skills/>
+        
+        </div>
+        
+    )
+}export default App
